c63a7413be301eb18ece259d82bee82c4638381b
moonumber8 <moo-hesoyam@hotmail.com>
2019-12-21 00:36:28 +0700

    commit code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  count = 0
+
+  
+  funOnClickCount(){
+    this.count = this.count + 1;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Com1Component } from './componentd/com1/com1.component';
+import { Com2Component } from './componentd/com2/com2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Com1Component,
+    Com2Component
   ],
   imports: [
     BrowserModule,
